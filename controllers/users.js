b147bd96b9553581a1e025d3fcf82ec8f61f1202
Valya <vshulya@gmail.com>
2022-05-19 12:45:47 -1000

    update user

--- controllers/users.js
+++ controllers/users.js
@@ -54,44 +54,36 @@ module.exports.createUser = (req, res, next) => {
     // данные не записались, вернём ошибку
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new ValidationError('Некорректные данные при создании пользователя'));
-      } else if (err.code === 11000) {
-        next(new ConflictError('Пользователь уже существует'));
-      } else {
-        next(new ServerError());
+        next(new ValidationError('Некорректные данные при создании карточки'));
       }
     });
 };
+
+// if (err.name === 'ValidationError') {
+//   next(new ValidationError('Некорректные данные при создании пользователя'));
+// } else if (err.code === 11000) {
+//   next(new ConflictError('Пользователь уже существует'));
+// } else {
+//   next(new ServerError());
+// }
 
 // PATCH /users/me — update profile
 module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
   // обновим имя найденного по _id пользователя
   User.findByIdAndUpdate(req.user._id, { name, about }, {
-    new: true,
-  }, { runValidators: true })
+    new: true, runValidators: true,
+  })
     .then((user) => {
       res.status(201).send(user);
     })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new ValidationError('Некорректные данные при обновлении пользователя'));
-      } else {
-        next(new ServerError());
-      }
-    });
+    .catch(next);
 };
 
 // PATCH /users/me/avatar — update avatar
 module.exports.updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new ValidationError('Некорректные данные при обновлении пользователя'));
-      } else {
-        next(new ServerError());
-      }
-    });
+    .catch(next);
 };
